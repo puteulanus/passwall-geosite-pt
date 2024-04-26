88ecdc0781f4cef1ed4738abafa436c7a1e29c2c
Puteulanus <mj19941230@gmail.com>
2024-04-26 14:05:08 +0000

    add build script

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 确保脚本出错时终止
+set -e
+
+# 定义编译目标平台
+PLATFORMS=("darwin/amd64" "darwin/arm64" "linux/amd64" "linux/arm64")
+
+# 获取当前目录名，用作输出文件名的一部分
+DIR_NAME=$(basename "$(pwd)")
+
+for platform in "${PLATFORMS[@]}"; do
+    # 分割字符串成数组
+    IFS="/" read -r -a os_arch <<< "$platform"
+
+    # 设置目标操作系统和体系结构
+    GOOS=${os_arch[0]}
+    GOARCH=${os_arch[1]}
+    OUTPUT_NAME="${DIR_NAME}-${GOOS}-${GOARCH}"
+
+    if [ "$GOOS" = "windows" ]; then
+        OUTPUT_NAME+='.exe'
+    fi
+
+    echo "Building for $GOOS/$GOARCH..."
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o "build/$OUTPUT_NAME"
+done
+
+echo "Build completed."
---
+++ go.mod
@@ -0,0 +1,10 @@
+module passwall-geosite-pt
+
+go 1.22.2
+
+require (
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.2.1-0.20190205222052-c823c79ea157 // indirect
+	go.starlark.net v0.0.0-20190225160109-1174b2613e82 // indirect
+	v2ray.com/core v4.19.1+incompatible // indirect
+)
